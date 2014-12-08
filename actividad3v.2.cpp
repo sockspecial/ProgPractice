1562bda36dd96f2cb051f7754c27a73cf1198bf8
embogh@gmail.com <embogh@gmail.com>
2014-12-08 22:57:26 +0100

    latest changes

--- actividad3v.2.cpp
+++ actividad3v.2.cpp
@@ -15,25 +15,55 @@ void dcCCC (char entbancaria[], char sucursal[], char cuenta[], char digcontrol[
 
 void escribeIBAN(char x[]);
 
+void dcIBAN (char cuenta[], char CodigoPais[]) {
+    //Anadimos las letras y los dos ceros al final.
+    strcat(strcat(cuenta,CodigoPais),"00");
+
+
+}
+
+void reemplazaLetra(char cadena[]) {
+    int tmax = strlen(cadena);
+    char storage[tmax];
+    char aux[2];
+    int numval;
+    for (int i=0;i<tmax;i++) {
+        numval = toInt(cadena[i]);
+        //Comprobamos si es el elemento i de cadena
+        //una letra entre A y Z mayusculas o no.
+        if (numval>=65 || numval<=90){
+            aux[0] = toChar((int)(numval/10));
+            aux[1] = toChar(numval%10);
+            strcat(storage,aux);
+
+        }
+    }
+
+
+}
+
+
+
 int main(void)
 {
 
     char entbancaria[5] = "8987";
     char sucursal[6]    = "9876";
-    char cuenta[11]     = "8888888888";
-    char digcontrol[3] = "";
-
-    dcCCC(entbancaria,sucursal,cuenta,digcontrol);
-
-    printf("%s",digcontrol);
+    char cuenta[20]     = "8888888888";
+    char digcontrol[3]  = "";
 
 
-   // cout << dcCCC(cadena) ;
+    double x = 4.99;
+    char a[2];
+
+    printf("%d", (int)x);
 
 
 
     return 0;
 }
+
+
 
 int toInt(char digito) {
     return digito - '0';
@@ -46,14 +76,14 @@ char toChar(int n) {
 void escribeIBAN(char IBAN[]) {
     int longIBAN = strlen(IBAN);
     for (int i=1;i<longIBAN+1;i++) {
-        cout << IBAN[i-1];
+        printf("%s",IBAN[i-1]);
         if (i % 4 == 0)
-            cout << " ";
+            printf(" ");
     }
 }
 
 int resto (char cadena[], int divisor) {
-    int r = 0;
+    int r  = 0;
     int longCadena = strlen(cadena);
     for (int k=0;k<longCadena;k++) {
         r  =  10*r+toInt(cadena[k]);
@@ -69,25 +99,24 @@ int dcCCCaux (char cadena[]) {
     int exp2       = 1;
     int resultado  = 0;
     for (int i=0;i<longCadena;i++) {
-        resultado += (toInt(cadena[i])*exp2)%11;
-        exp2       = exp2*2;
+        resultado += (toInt(cadena[i]) * exp2) % 11;
+        exp2      *= 2;
     }
-    resultado %= 11;
-    cout << "Resultado: " << resultado << endl;
+    resultado     %= 11;
     return resultado;
 }
 
 void dcCCC (char entbancaria[], char sucursal[], char cuenta[], char digcontrol[]) {
     char a, b;
 
-    /*calculo del segundo digito de control*/
+    //calculo del segundo digito de control
     int x = dcCCCaux(cuenta);
     int valor = 11 - (x % 11);
     if      (valor  < 10)   b = toChar(valor);
     else if (valor == 10)   b = '1';
     else                    b = '0';
 
-    /*calculos el primer digito de control*/
+    //calculos el primer digito de control
     char cadenaAux[3] = {'0','0','\0'};
     x = dcCCCaux(strcat(strcat(cadenaAux,entbancaria),sucursal));
     valor = 11 - (x % 11);
@@ -95,7 +124,7 @@ void dcCCC (char entbancaria[], char sucursal[], char cuenta[], char digcontrol[
     else if (valor == 10)   a = '1';
     else                    a = '0';
 
-    /*almacenamos los resultados obtenisdos*/
+    //almacenamos los resultados obtenidos
     digcontrol[0]= a;
     digcontrol[1]= b;
 }
